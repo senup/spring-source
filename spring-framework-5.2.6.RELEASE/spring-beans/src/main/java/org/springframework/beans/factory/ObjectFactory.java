0dd572f505421c35f66c5a7dc0ed13e29aa1b073
butcher <senup@foxmail.com>
2023-12-25 17:31:47 +0800

    source

---
+++ spring-framework-5.2.6.RELEASE/spring-beans/src/main/java/org/springframework/beans/factory/ObjectFactory.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2002-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.beans.factory;
+
+import org.springframework.beans.BeansException;
+
+/**
+ * Defines a factory which can return an Object instance
+ * (possibly shared or independent) when invoked.
+ *
+ * <p>This interface is typically used to encapsulate a generic factory which
+ * returns a new instance (prototype) of some target object on each invocation.
+ *
+ * <p>This interface is similar to {@link FactoryBean}, but implementations
+ * of the latter are normally meant to be defined as SPI instances in a
+ * {@link BeanFactory}, while implementations of this class are normally meant
+ * to be fed as an API to other beans (through injection). As such, the
+ * {@code getObject()} method has different exception handling behavior.
+ *
+ * @author Colin Sampaleanu
+ * @since 1.0.2
+ * @param <T> the object type
+ * @see FactoryBean
+ */
+@FunctionalInterface
+public interface ObjectFactory<T> {
+
+	/**
+	 * Return an instance (possibly shared or independent)
+	 * of the object managed by this factory.
+	 * @return the resulting instance
+	 * @throws BeansException in case of creation errors
+	 */
+	//只有一个待实现的接口方法，使用了普通工厂的设计模式。
+	// 默认的Bean是走这个接口，如果是factoryBean，那么也会走下下面这个工厂方法，但是最终还是以factorybean创建的单例Bean为主
+	T getObject() throws BeansException;
+
+}
